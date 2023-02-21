8c36c31d172931561076a692d978ed6254f03073
Leonid Scott <marmostro-iv-ii@Leonids-MacBook-Pro.local>
2023-02-21 14:30:39 -0500

    Git plotly working quick and dirty

--- src/leonidscott/github/io/core.cljs
+++ src/leonidscott/github/io/core.cljs
@@ -1,19 +1,34 @@
 (ns leonidscott.github.io.core
     (:require
       [reagent.core :as r]
-      [reagent.dom :as d]))
+      [reagent.dom :as d]
+      ["plotly.js-dist-min" :as plotly]))
 
 ;; -------------------------
 ;; Views
 
 (defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+  [:div
+   [:h2 "Welcome to Reagent"]
+   [:div {:id "graph" :style {:width "600px" :height "250px"}}]])
 
 ;; -------------------------
 ;; Initialize app
 
+(defn clj->json
+  [ds]
+  (.stringify js/JSON (clj->js ds)))
+
+(defn initalize-graph! []
+  (let [data [{:x [1, 2, 3, 4, 5]
+               :y [1, 2, 4, 8, 16]}]
+        margin {:t 0}
+        graph (-> js/document (.getElementById "graph"))]
+    (. plotly newPlot graph (clj->js data) (clj->js margin))))
+
 (defn mount-root []
-  (d/render [home-page] (.getElementById js/document "app")))
+  (d/render [home-page] (.getElementById js/document "app"))
+  (initalize-graph!))
 
 (defn ^:export init! []
   (mount-root))
