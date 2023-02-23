ff610633dfadc548b6475bcf3ea87156a8c28555
Leonid Scott <marmostro-iv-ii@Leonids-MacBook-Pro.local>
2023-02-23 14:43:47 -0500

    graph -> plot

--- src/leonidscott/github/io/plot.cljs
+++ src/leonidscott/github/io/plot.cljs
@@ -7,16 +7,16 @@
   [ds]
   (.stringify js/JSON (clj->js ds)))
 
-(defn initalize-graph! []
+(defn initalize-plot! []
   (let [data   [{:x [1, 2, 3, 4, 5]
                  :y [1, 2, 4, 8, 16]}]
         margin {:t 0}
-        graph  (-> js/document (.getElementById "graph"))]
-    (. plotly newPlot graph (clj->js data) (clj->js margin))))
+        plot  (-> js/document (.getElementById "plot"))]
+    (. plotly newPlot plot (clj->js data) (clj->js margin))))
 
 (defn plot []
   (r/create-class
     {:reagent-render
-     (fn [] [:div {:id "graph" :style {:width "600px" :height "250px"}}])
+     (fn [] [:div {:id "plot" :style {:width "600px" :height "250px"}}])
 
-     :component-did-mount initalize-graph!}))
+     :component-did-mount initalize-plot!}))
