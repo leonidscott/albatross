f38521c205b32aeb5b2f4e9a845c3b2f4deeb9bb
Leonid Scott <marmostro-iv-ii@Leonids-MacBook-Pro.local>
2023-03-09 15:13:39 -0500

    Add intercepts wrapper for reg-event-fx

--- src/leonidscott/github/io/events.cljs
+++ src/leonidscott/github/io/events.cljs
@@ -4,3 +4,9 @@
    [re-frame.core :as re-frame]))
 
 (re-frame/reg-event-db ::initialize-db (fn [_ _] db/default-db))
+
+(defn reg-event-fx
+  ([id handler]
+   (reg-event-fx id nil handler))
+  ([id interceptors handler]
+   (re-frame/reg-event-fx id interceptors handler)))
